49aeb313d9352cb2cb995280ed9903cf5bd5d82c
Phani-8649 <120397247+Phani-8649@users.noreply.github.com>
2023-06-23 22:37:40 +0530

    Add files via upload

---
+++ src/app.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+ function App(){
+    return(
+        <div>
+            <div>
+                <h1>Student login</h1>
+                
+            </div>
+        </div>
+    )
+ }
+ export default App;
